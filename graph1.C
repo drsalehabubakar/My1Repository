f83d753e98a65beec0e8b5281ae39b296b9cf833
drsalehabubakar <138799229+drsalehabubakar@users.noreply.github.com>
2023-10-09 16:41:45 +0300

    Add files via upload

---
+++ graph1.C
@@ -0,0 +1,13 @@
+// Example of reading data file and plotting
+
+/*{
+
+g = new TGraph ("data.txt");
+g->SetMarkerStyle(21);
+g->SetLineColor(kRed);
+g->SetTitle("Data Plot with TGRAPH;GW;ms");
+g->Draw("ALP");
+
+
+}*/
+
